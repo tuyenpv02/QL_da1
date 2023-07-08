1e4e40865a102b9d141cf27ea24a44b13ba4b083
tuyenpv02 <tuyenphamk202@gmail.com>
2023-07-08 23:07:15 +0700

    push sql

---
+++ database/sql.sql
@@ -0,0 +1,14 @@
+﻿
+
+select (select count(d.ma) from  HoaDon d  
+                where year(ngaytao) = 2023 and month(ngaytao) =7
+                ) as 'tổng HD',
+				( select count( ma)  from HoaDon d   where trangthai = 1
+                and year(ngaytao) = 2023 and month(ngaytao) =7 ) as 'thanh cong',
+                (select count(trangthai) from  HoaDon d  where trangthai = 2 and year(ngaytao) = 2023 
+                and month(ngaytao) =7 ) as 'huy'
+				,sum(hd.soluong*hd.dongia)  as [doanh thu]
+                from HoaDonChiTiet HD  inner join HoaDon d on d.id=hd.IdHoaDon 
+                 inner join ChiTietSP ct on ct.Id=hd.IdChiTietSP
+                where  year(ngaytao) = 2023 and month(ngaytao) =7 and d.trangthai= 1
+
