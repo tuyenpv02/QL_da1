6277be4843893ba0f86e41e693be9f89013d951f
tuyenpv02 <tuyenphamk202@gmail.com>
2023-07-21 12:02:33 +0700

    update

--- du_an1/demo_du_an/src/Repositories/HoaDonRepository.java
+++ du_an1/demo_du_an/src/Repositories/HoaDonRepository.java
@@ -241,20 +241,20 @@ public class HoaDonRepository {
     }
     
     public ArrayList<HoaDonViewModels> locTrangThai(int txt) {
-        String sql = "select hd.Ma as mahd, hd.NgayTao as ngaytao,\n" +
-"hd.NgayThanhToan as NgayThanhToan, nv.Ma as maNV, kh.Ma as makh,\n" +
-"sum(hdct.SoLuong * hdct.DonGia) as 'TongTien'\n" +
-", hd.TrangThai as TrangThai, hd.GhiChu as ghiChu\n" +
-"from HoaDon hd left join NhanVien nv on nv.id=hd.IdNV\n" +
-"left join KhachHang kh on hd.IdKH=kh.Id\n" +
-"left join HoaDonChiTiet hdct on hd.id=hdct.IdHoaDon\n" +
-"left join ChiTietSP ctsp on hdct.IdChiTietSP=ctsp.id\n" +
-"left join SanPham sp on ctsp.IdSP=sp.id\n" +
-"  where hd.TrangThai = ? \n" +
-"group by hd.Ma, hd.NgayTao, hd.NgayThanhToan, nv.Ma, kh.Ma, hd.TrangThai, hd.GhiChu";
+        String sql = "select hd.Ma as mahd, hd.NgayTao as ngaytao,\n"
+                + "hd.NgayThanhToan as NgayThanhToan, nv.Ma as maNV, kh.Ma as makh,\n"
+                + "sum(hdct.SoLuong * hdct.DonGia) as 'TongTien'\n"
+                + ", hd.TrangThai as TrangThai, hd.GhiChu as ghiChu\n"
+                + "from HoaDon hd left join NhanVien nv on nv.id=hd.IdNV\n"
+                + "left join KhachHang kh on hd.IdKH=kh.Id\n"
+                + "left join HoaDonChiTiet hdct on hd.id=hdct.IdHoaDon\n"
+                + "left join ChiTietSP ctsp on hdct.IdChiTietSP=ctsp.id\n"
+                + "left join SanPham sp on ctsp.IdSP=sp.id\n"
+                + "  where hd.TrangThai = ? \n"
+                + "group by hd.Ma, hd.NgayTao, hd.NgayThanhToan, nv.Ma, kh.Ma, hd.TrangThai, hd.GhiChu";
         ArrayList<HoaDonViewModels> ds = new ArrayList<>();
         try (Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-             ps.setObject(1, txt);
+            ps.setObject(1, txt);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 HoaDonViewModels h = new HoaDonViewModels();
